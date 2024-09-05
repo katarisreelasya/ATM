e0af4f459658e466924702f3f165e572fe2da0db
katarisreelasya25@gmail.com <katarisreelasya25@gmail.com>
2024-09-05 17:18:05 +0530

    ATM

---
+++ ATM.java
@@ -0,0 +1,78 @@
+ import java.util.Scanner;  
+    
+public class ATM 
+{  
+  public static boolean validateInputAmount(Integer amount)
+  {
+    return amount>0;
+  }
+       
+    public static void main(String args[] )  
+    {  
+        System.out.println("Welcome to the ATM!");
+        int balance = 0, withdraw, deposit;  
+           
+        Scanner sc = new Scanner(System.in);  
+          
+        while(true)  
+        {  
+            System.out.println("Automated Teller Machine");  
+            System.out.println("Choose 1 for Withdraw");  
+            System.out.println("Choose 2 for Deposit");  
+            System.out.println("Choose 3 for Check Balance");  
+            System.out.println("Choose 4 for EXIT");  
+            System.out.print("Choose the operation you want to perform:");  
+               
+            int choice = sc.nextInt();  
+            switch(choice)  
+            {  
+                case 1:  
+        System.out.print("Enter money to be withdrawn:");  
+                       
+        withdraw = sc.nextInt(); 
+                if(!validateInputAmount(withdraw))
+                {
+                  System.out.println("Enter amount greater than zero");
+                  break;
+                }
+                      
+        if(balance >= withdraw)  
+        {    
+            balance = balance - withdraw;  
+            System.out.println("Please collect your money");  
+        }  
+        else  
+        {    
+            System.out.println("Insufficient Balance");  
+        }  
+        System.out.println("");  
+        break;  
+   
+                case 2:  
+                      
+        System.out.print("Enter money to be deposited:");  
+                       
+        deposit = sc.nextInt();  
+                 if(!validateInputAmount(deposit))
+                {
+                  System.out.println("Enter amount greater than zero");
+                  break;
+                }
+                        
+        balance = balance + deposit;  
+        System.out.println("Your Money has been successfully depsited");  
+        System.out.println("");  
+        break;  
+   
+                case 3:  
+        
+        System.out.println("Your current Balance : "+balance);  
+        System.out.println("");  
+        break;  
+   
+                case 4:  
+        System.exit(0);  
+            }  
+        }  
+    }  
+}  
